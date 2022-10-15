c784abd6960077fe04cd36ce65383b5fe30fee27
Ahmed Salim <ahmed.salim@adamtravel.net>
2022-10-15 17:31:32 +0200

    add db tables

---
+++ database/migrations/2022_10_15_144540_create_store_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoreSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('value')->nullable();
+            $table->foreignId('store_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('store_settings');
+    }
+}
